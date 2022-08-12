b3e7783ac55f4d67b094ba67670e5c2bf89a7ee8
ivkn31 <ivkn31@gmail.com>
2022-08-12 22:51:57 +0700

    feat: ui components, getUsers, pagination

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ManyPageContainer from './Components/ManyPage/ManyPageContainer';
+import { Provider } from 'react-redux';
+import store from './redux/reduxStore';
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Provider store={store}>
+    <ManyPageContainer/>
+  </Provider>
   </React.StrictMode>
 );
 
